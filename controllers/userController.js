2124cf4777b92aaf040bb8ceb1813a58a979aec0
acadomate <edu.acadomate@gmail.com>
2023-04-02 19:43:46 +0530

    Logout new Fixeddd

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,11 +67,9 @@ export const login = catchAsyncError(async(req,res,next)=>{
 
 //logout
 export const logout = catchAsyncError(async (req,res,next) =>{
-    res.status(200).cookie("token",null,{
+    res.cookie("token",null,{
         expires:new Date(Date.now()),
         httpOnly:true,
-        secure:true,
-        sameSite:"none",
     })
     .json({
         success:true,
